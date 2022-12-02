7b4f34b2429f1b209eb266939bb5b4754f90c3cd
Zen Serendip <zenserendip@gmail.com>
2022-12-03 00:14:56 +0300

    edit code in version2

--- guess-number/game_v2.py
+++ guess-number/game_v2.py
@@ -4,8 +4,8 @@
 
 import numpy as np
 
-def random_predict(number: int = 1) -> int:
-    """Рандомно угадываем число
+def avg_predict(number: int = 1) -> int:
+    """Угадываем число по алгоритму деления на 2
 
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
@@ -15,29 +15,37 @@ def random_predict(number: int = 1) -> int:
     """
     count = 0
 
+    range_min = 0
+    range_max = 101
+
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101)  # предполагаемое число
-        if number == predict_number:
-            break  # выход из цикла если угадали
+        avg_num = int((range_max + range_min) / 2)
+        if number < avg_num:
+            range_max = avg_num
+        elif number > avg_num:
+            range_min = avg_num
+        else:
+            break
+
     return count
 
 
-def score_game(random_predict) -> int:
+def score_game(avg_predict) -> int:
     """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
 
     Args:
-        random_predict ([type]): функция угадывания
+        avg_predict ([type]): функция угадывания
 
     Returns:
         int: среднее количество попыток
     """
     count_ls = []
-    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    np.random.seed()  # фиксируем сид для воспроизводимости
     random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
 
     for number in random_array:
-        count_ls.append(random_predict(number))
+        count_ls.append(avg_predict(number))
 
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
@@ -46,4 +54,4 @@ def score_game(random_predict) -> int:
 
 if __name__ == "__main__":
     # RUN
-    score_game(random_predict)
+    score_game(avg_predict)
